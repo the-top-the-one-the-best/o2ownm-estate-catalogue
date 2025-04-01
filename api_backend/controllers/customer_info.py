e2874b5d226f7da7304a65ff67b38183e79c436e
alexchiu <tainancgw@gmail.com>
2025-04-01 15:18:01 +0800

    update

--- api_backend/controllers/customer_info.py
+++ api_backend/controllers/customer_info.py
@@ -5,15 +5,15 @@ from api_backend.services.customer_info import CustomerInfoService
 
 name = __name__.replace(".", "_")
 blueprint = flask.Blueprint(name, __name__)
-alumni_info_service = CustomerInfoService()
+customer_info_service = CustomerInfoService()
 
 @blueprint.route("/admin/query", methods=["GET"])
 @doc(
-  summary='admin query alumni member info by dto',
+  summary='admin query customer info by dto',
   tags=['客戶資料', 'admin'],
 )
 @use_kwargs(QueryCustomerInfoDto, location="query")
 @marshal_with(PagedCustomerInfoDto)
 def query(**query):
-  result = alumni_info_service.query_by_filter(query)
+  result = customer_info_service.query_by_filter(query)
   return flask.jsonify(PagedCustomerInfoDto().dump(result))
