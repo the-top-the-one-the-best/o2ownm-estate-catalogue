2c70701e2d441715999e8dc6f4f9e27f945ee3ea
alexchiu <tainancgw@gmail.com>
2025-05-20 15:37:52 +0800

    update

--- api_backend/task_function/estate_customer_info_import.py
+++ api_backend/task_function/estate_customer_info_import.py
@@ -124,6 +124,7 @@ def import_customer_xlsx_to_draft(task, mongo_client=None):
         else:
           data[field_name] = value
       elif field_name == "room_sizes" and value and value.strip():
+        value = value.replace("坪", "")
         size_ranges = []
         data[field_name] = size_ranges
         for size_range in str(value).split(","):
