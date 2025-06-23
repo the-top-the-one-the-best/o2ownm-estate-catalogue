a76c57bf4bec9dd58e7ce8606c2182a0678bfbc6
alexchiu <tainancgw@gmail.com>
2025-06-23 17:11:43 +0800

    update-export-blacklist-exclusion-logic

--- api_backend/task_function/estate_customer_info_export.py
+++ api_backend/task_function/estate_customer_info_export.py
@@ -1,4 +1,5 @@
 import pymongo
+from api_backend.services.customer_blacklist import CustomerBlacklistService
 from api_backend.services.customer_info import CustomerInfoService
 from config import Config
 from openpyxl import Workbook
@@ -15,6 +16,11 @@ def export_customer_xlsx(task, mongo_client=None):
     mongo_client = pymongo.MongoClient(Config.MONGO_MAIN_URI)
 
   customer_info_service = CustomerInfoService(mongo_client=mongo_client)
+  customer_blacklist_service = CustomerBlacklistService(mongo_client=mongo_client)
+  blacklist_phones = set(
+    entry.get("phone")
+    for entry in customer_blacklist_service.collection.find({})
+  )
   export_cursor = customer_info_service.query_export_cursor(filter)
 
   # start writing
@@ -26,7 +32,8 @@ def export_customer_xlsx(task, mongo_client=None):
   i = 0
   for entry in export_cursor:
     i += 1
-    ws.append([entry[field] for field in CUSTOMER_XLSX_EXPORT_FIELD_HEADER_MAP])
+    if entry.get("phone") not in blacklist_phones:
+      ws.append([entry[field] for field in CUSTOMER_XLSX_EXPORT_FIELD_HEADER_MAP])
 
   # Save the workbook
   wb.save(file_path)
